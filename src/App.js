7106f69e84dc68db682cf26fd98ecb264b9252a6
Hudson Chamberlain <hudson.m.chamberlain@gmail.com>
2020-02-05 09:34:47 -0600

    hide show complete, render correct icon

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import NewJobDialog from './components/NewJobDialog';
-import {JPSignUp} from './components/JPSignUp';
+import { MenuNav } from './components/dashboard/MenuNav'; 
 
 function App() {
-
-  const modalStyles = {
-    content: {
-      maxWidth: "500px",
-      paddingBottom: "25px",
-
-      margin: "0 auto",
-      verticalAlign: "middle",
-
-      marginTop: '50px'
-    }
-  }
-
-  const [open, setOpen] = React.useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-      setOpen(false);
-  };
-
   return (
     <div className="App">
-      <NewJobDialog />
-      <br />
-      <br />
-      <JPSignUp />
+      <MenuNav />
     </div>
   );
 }
