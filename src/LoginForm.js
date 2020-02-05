a14108c7477e65c8378e15fc40a0e3b9e48d8b24
occludedarp <cmrnmr@gmail.com>
2020-02-05 13:53:08 -0500

    updated mui switch removed react-bootstrap

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { Button, FormGroup, FormControl } from "react-bootstrap";
 import cogoToast from "cogo-toast";
-import "./loginForm.css";
+// import "./loginForm.css";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import Switch from '@material-ui/core/Switch';
 
 const { hide } = cogoToast.success('Success. ', {
@@ -10,11 +11,18 @@ const { hide } = cogoToast.success('Success. ', {
   }
 })
 
-
 export default function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const [Login, setLogin] = useState({
+    Seeker: true,
+    Provider: true
+  })
+
+  const [state, setState] = React.useState({
+    checkedA: true,
+  });
 
   function validationForm() {
     return email.length > 0 && password > 0;
@@ -24,36 +32,90 @@ export default function LoginForm() {
     e.preventDefault();
   }
 
-  return (
-    <div className="LoginForm">
-      <h1> Welcome to Droom </h1>
+  const handleChange = name => event => {
+    setState({ ...state, [name]: event.target.checked });
+    console.log(event.target.checked)
+    console.log(Login)
+    
+  };
 
-      <form onSubmit={handleSubmit}>
-        <FormGroup controlId="email">
-          <label> Email </label>
-          <br />
-          <FormControl
-            autoFocus
-            type="email"
-            autoComplete="new-email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
+  return state.checkedA 
+    ? 
+      (
+      <div className="LoginForm">
+        <h1> Seeker Login </h1>
+
+        <form onSubmit={handleSubmit}>
+          <div controlId="email">
+            <label> Email </label>
+            <br />
+            <TextField
+              autoFocus
+              type="email"
+              autoComplete="new-email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+          </div>
+          <div controlId="password">
+            <label> Password </label>
+            <br />
+            <TextField
+              type="password"
+              autoComplete="new-password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+          <Button  variant="outlined" disable={`${!validationForm()}`} type="submit">
+            Login
+          </Button>
+        </form>
+
+          <Switch
+          checked={state.checkedA, Login.Provider}
+          onChange={handleChange('checkedA')}
+          value="checkedA"
+          inputProps={{ 'aria-label': 'secondary checkbox' }}
           />
-        </FormGroup>
-        <FormGroup controlId="password">
-          <label> Password </label>
-          <br />
-          <FormControl
-            type="password"
-            autoComplete="new-password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
+      </div>)
+    :
+      (
+      <div className="LoginForm">
+        <h1> Recruiter Login</h1>
+
+        <form onSubmit={handleSubmit}>
+          <div controlId="email">
+            <label> Email </label>
+            <br />
+            <TextField
+              autoFocus
+              type="email"
+              autoComplete="new-email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+          </div>
+          <div controlId="password">
+            <label> Password </label>
+            <br />
+            <TextField
+              type="password"
+              autoComplete="new-password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+          <Button variant="outlined" disable={`${!validationForm()}`} type="submit">
+            Login
+          </Button>
+        </form>
+
+          <Switch
+          checked={state.checkedA}
+          onChange={handleChange('checkedA')}
+          value="checkedA"
+          inputProps={{ 'aria-label': 'secondary checkbox' }}
           />
-        </FormGroup>
-        <Button  disable={`${!validationForm()}`} type="submit">
-          Login
-        </Button>
-      </form>
-    </div>
-  );
+      </div>)
 }
