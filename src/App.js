08072e1b24812f8c0a08436bc993a946d67bbd16
occludedarp <cmrnmr@gmail.com>
2020-02-05 17:14:35 -0500

    private route completed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import LoginForm from './components/LoginForm';
 import JobSeekerOnboarding from './components/JobSeekerOnboarding';
 import { JPSignUp } from './components/JPSignUp';
 import { Dashboard } from './components/dashboard/Dashboard';
-
+import PrivateRoute from './components/PrivateRoute';
 import {Route, Switch} from 'react-router-dom';
 
 
@@ -14,9 +14,9 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path='/' render={ props => <LoginForm {...props}/> }/>
-        <Route path='/sign-up-seeker' render={ props => <JobSeekerOnboarding {...props} /> }/>
-        <Route path='/sign-up-provider' render={ props => <JPSignUp {...props}/> } />
-        <Route path='/dashboard' render={props => <Dashboard />} />
+        <Route path='/sign-up-seeker' component={JobSeekerOnboarding} />
+        <Route path='/sign-up-provider' component={JPSignUp} />
+        <PrivateRoute path='/dashboard' component={Dashboard} />
       </Switch>
     </div>
   );
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -6,12 +6,6 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Switch from '@material-ui/core/Switch';
 
-const { hide } = cogoToast.success('Success. ', {
-  onClick: () => {
-    hide();
-  }
-})
-
 export default function LoginForm(props) {
 
   const [credentials, setCredentials] = useState({
